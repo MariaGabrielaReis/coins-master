8d5d32c1a9d678d9b12f535e70909479f823f74f
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-09-09 16:10:21 -0300

    feat: add fonts and spacing tokens

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -14,4 +14,28 @@ export default {
     good: "#23BBA0",
     bad: "#FF5252",
   },
+  fonts: {
+    title: "600 24px/29px sans-serif",
+    subtitle: "400 16px/20.5px sans-serif",
+    button: "400 14px/29px sans-serif",
+    text: "400 14px/29px sans-serif",
+    navbar: "400 18px/24px sans-serif",
+    navbarLink: "400 12px/24px sans-serif",
+    cardTitle: "400 16px/9px sans-serif",
+    cardSubtitle: "400 12px/9px sans-serif",
+    hability: "400 16px/9px sans-serif",
+  },
+  spacing: {
+    xxs: "4px",
+    xs: "8px",
+    sm: "16px",
+    md: "24px",
+    lg: "32px",
+    xl: "48px",
+    xxl: "56px",
+  },
+  borderRadius: {
+    sm: "4px",
+    md: "8px",
+  },
 };

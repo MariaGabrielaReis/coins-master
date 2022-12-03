e6e5e505169027691ec57ec91da883c380439c2c
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-12-03 11:19:44 -0300

    feat: remove unused code

--- src/screens/ConfigurateTeam/index.tsx
+++ src/screens/ConfigurateTeam/index.tsx
@@ -6,25 +6,16 @@ import {
   BlackButton,
   LabelButton,
   YellowButton,
-  TextInput,
   Habilities,
 } from "@components";
 
 import { MainContext } from "@context";
-import { updateTeam } from "@requests/TeamRequests";
-import { Team } from "@interfaces/Team";
-import { setMembers, setTeam } from "@reducer";
-import { User } from "@interfaces/User";
 
 export default function ConfigurateTeam() {
   const navigation = useNavigation();
   const {
     state: { team },
-    dispatch,
   } = useContext(MainContext);
-
-  const [name, setName] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
 
   const [abilities, setAbilities] = useState([
     team?.ability1 ?? "Proatividade",
@@ -34,53 +25,22 @@ export default function ConfigurateTeam() {
     team?.ability5 ?? "Carisma",
   ]);
 
-  function handleUpdate() {
-    setIsLoading(true);
-    updateTeam(
-      team.name,
-      name ?? team.name,
-      team.classroom,
-      abilities,
-      navigation,
-    ).then((response: { team: Team; members: User[] }) => {
-      dispatch(setTeam(response.team));
-      dispatch(setMembers(members));
-      setIsLoading(false);
-      navigation.navigate("Home");
-    });
-  }
-
   return (
     <Container>
-      <TextInput
-        label={"Nome"}
-        placeholder={team.name}
-        value={name}
-        onChange={name => setName(name)}
-      />
-      <BlackButton
-        label={"ADICIONAR MOEDAS"}
-        isDisabled={isLoading}
-        onClick={() => navigation.navigate("AddCoins")}
-      />
-      <LabelButton
-        label={"ADICIONAR INTEGRANTE"}
-        isDisabled={isLoading}
-        onClick={() => navigation.navigate("AddMember")}
-      />
       <Habilities
         defaultValue={abilities}
         handleOnChange={(abilities: string[]) => setAbilities(abilities)}
       />
-
+      <BlackButton
+        label={"ADICIONAR MOEDAS"}
+        onClick={() => navigation.navigate("AddCoins")}
+      />
       <YellowButton
-        label={isLoading ? "Aguarde..." : "ATUALIZAR"}
-        isDisabled={isLoading}
-        onClick={handleUpdate}
+        label={"ADICIONAR INTEGRANTE"}
+        onClick={() => navigation.navigate("AddMember")}
       />
       <LabelButton
         label={"DELETAR EQUIPE"}
-        isDisabled={isLoading}
         onClick={() => navigation.navigate("DeleteTeam")}
       />
     </Container>
--- src/screens/CreateTeam/index.tsx
+++ src/screens/CreateTeam/index.tsx
@@ -2,13 +2,7 @@ import React, { useState, useContext } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 import { Container } from "./styles";
-import {
-  BlackButton,
-  Dropdown,
-  TextInput,
-  Habilities,
-  OptionProps,
-} from "@components";
+import { BlackButton, TextInput, Habilities, OptionProps } from "@components";
 
 import { createTeam } from "@requests/TeamRequests";
 import { MainContext } from "@context";
@@ -30,28 +24,6 @@ export default function CreateTeam() {
     "Resiliência",
   ]);
 
-  const semesters: OptionProps[] = [
-    { label: "Selecione uma opção", value: "" },
-    { label: "1ºDSM", value: "1dsm" },
-    { label: "2ºDSM", value: "2dsm" },
-    { label: "3ºDSM", value: "3dsm" },
-    { label: "4ºDSM", value: "4dsm" },
-    { label: "5ºDSM", value: "5dsm" },
-    { label: "6ºDSM", value: "6dsm" },
-    { label: "1ºADS", value: "1ads" },
-    { label: "2ºADS", value: "2ads" },
-    { label: "3ºADS", value: "3ads" },
-    { label: "4ºADS", value: "4ads" },
-    { label: "5ºADS", value: "5ads" },
-    { label: "6ºADS", value: "6ads" },
-    { label: "1ºBD", value: "1bd" },
-    { label: "2ºBD", value: "2bd" },
-    { label: "3ºBD", value: "3bd" },
-    { label: "4ºBD", value: "4bd" },
-    { label: "5ºBD", value: "5bd" },
-    { label: "6ºBD", value: "6bd" },
-  ];
-
   function handleCreateTeam() {
     setIsLoading(true);
     createTeam(name, classroom, abilities, navigation).then((newTeam: Team) => {
@@ -69,28 +41,12 @@ export default function CreateTeam() {
         value={name}
         onChange={name => setName(name)}
       />
-      {/* <TextInput
-        label={"Código da equipe"}
-        placeholder={"Exemplo: tbd2022"}
-        supportText={"(obs.: apenas letras minúsculas e números)"}
-        value={teamCode}
-        onChange={teamCode => seTeamCode(teamCode)}
-      /> */}
       <TextInput
         label={"Turma"}
         placeholder={"Exemplo: 4º DSM"}
         value={classroom}
         onChange={classroom => setClassroom(classroom)}
       />
-      {/* <Dropdown
-        label={"Turma"}
-        items={semesters}
-        open={open}
-        value={classroom}
-        setOpen={setOpen}
-        setValue={setClassroom}
-        hasSmallSpacing={true}
-      /> */}
       <Habilities
         defaultValue={abilities}
         handleOnChange={(abilities: string[]) => setAbilities(abilities)}
